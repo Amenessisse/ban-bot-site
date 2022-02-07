9d1ba23a0d32e346820794097cde19f7c4becdad
Amenessisse <amenessisse@gmail.com>
2022-02-07 22:37:16 +0100

    feat(dependencies): add vuejs, tailwinds, postcss, purgecss, saascss

---
+++ assets/vue/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>This is an example of a new components in VueJs test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testExample',
+};
+</script>
+
+<style scoped>
+
+</style>
