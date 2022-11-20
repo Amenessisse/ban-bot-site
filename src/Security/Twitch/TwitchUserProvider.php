67a3b8cce756ee7bf5f759796d478b689dfaf95b
Amenessisse <amenessisse@gmail.com>
2022-11-20 11:29:07 +0100

    fix(api): refresh token user

--- src/Security/Twitch/TwitchUserProvider.php
+++ src/Security/Twitch/TwitchUserProvider.php
@@ -8,14 +8,10 @@ use App\Entity\UserTwitch;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -24,13 +20,13 @@ use function is_subclass_of;
 class TwitchUserProvider implements UserProviderInterface
 {
     public function __construct(
-        private readonly HttpClientInterface    $apiTwitch,
-        private readonly HttpClientInterface    $identityTwitch,
-        private readonly string                 $twitchId,
-        private readonly string                 $twitchSecret,
-        private readonly UrlGeneratorInterface  $urlGenerator,
+        private readonly HttpClientInterface $apiTwitch,
+        private readonly HttpClientInterface $identityTwitch,
+        private readonly string $twitchId,
+        private readonly string $twitchSecret,
+        private readonly UrlGeneratorInterface $urlGenerator,
         private readonly EntityManagerInterface $entityManager,
-        private readonly UserRepository         $userRepository,
+        private readonly UserRepository $userRepository,
     ) {
     }
 
@@ -88,43 +84,8 @@ class TwitchUserProvider implements UserProviderInterface
         return $user;
     }
 
-    /**
-     * @throws RedirectionExceptionInterface
-     * @throws DecodingExceptionInterface
-     * @throws ClientExceptionInterface
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     */
     public function refreshUser(UserInterface $user): UserInterface
     {
-        if ($user instanceof UserTwitch && $user->getAccessToken() !== null) {
-            $response = $this->identityTwitch->request(
-                method: 'GET',
-                url: 'oauth2/validate',
-                options: ['auth_bearer' => $user->getAccessToken()],
-            );
-
-            if ($response->getStatusCode() === Response::HTTP_UNAUTHORIZED && $user->getRefreshToken() !== null) {
-                $dataToken = $this->identityTwitch->request(method: 'POST', url: 'oauth2/token', options: [
-                    'headers' => ['Accept' => 'application/json'],
-                    'query' => [
-                        'client_id' => $this->twitchId,
-                        'client_secret' => $this->twitchSecret,
-                        'grant_type' => 'refresh_token',
-                        'refresh_token' => $user->getRefreshToken(),
-                    ],
-                ]);
-
-                $token        = $dataToken->toArray()['access_token'];
-                $refreshToken = $dataToken->toArray()['refresh_token'];
-
-                $user->setAccessToken($token);
-                $user->setRefreshToken($refreshToken);
-                $this->entityManager->persist($user);
-                $this->entityManager->flush();
-            }
-        }
-
         return $user;
     }
 
--- src/Service/ApiTwitch.php
+++ src/Service/ApiTwitch.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\UserTwitch;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -13,9 +20,51 @@ class ApiTwitch
 {
     public function __construct(
         private readonly HttpClientInterface $apiTwitch,
+        private readonly HttpClientInterface $identityTwitch,
         private readonly Security $security,
         private readonly string $twitchId,
+        private readonly EntityManagerInterface $entityManager,
     ) {
+    }
+
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function checkValidityRequest(UserTwitch $user): UserTwitch
+    {
+        if ($user->getAccessToken() !== null) {
+            $response = $this->identityTwitch->request(
+                method: 'GET',
+                url: 'oauth2/validate',
+                options: ['auth_bearer' => $user->getAccessToken()],
+            );
+
+            if ($response->getStatusCode() === Response::HTTP_UNAUTHORIZED && $user->getRefreshToken() !== null) {
+                $dataToken = $this->identityTwitch->request(method: 'POST', url: 'oauth2/token', options: [
+                    'headers' => ['Accept' => 'application/json'],
+                    'query' => [
+                        'client_id' => $this->twitchId,
+                        'client_secret' => $this->twitchSecret,
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => $user->getRefreshToken(),
+                    ],
+                ]);
+
+                $token        = $dataToken->toArray()['access_token'];
+                $refreshToken = $dataToken->toArray()['refresh_token'];
+
+                $user->setAccessToken($token);
+                $user->setRefreshToken($refreshToken);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
+        }
+
+        return $user;
     }
 
     /**
@@ -25,13 +74,11 @@ class ApiTwitch
      */
     public function get(string $url, array $query): ResponseInterface
     {
-        $result = $this->apiTwitch->request(method: 'GET', url: $url, options: [
+        return $this->apiTwitch->request(method: 'GET', url: $url, options: [
             'auth_bearer' => $this->security->getUser()?->getaccessToken(),
             'headers' => ['Client-Id' => $this->twitchId],
             'query' => $query,
         ]);
-
-        return $result;
     }
 
     /**
